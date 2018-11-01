768f3a802eb3109ebd0423063c80208e12933c4d
Daniel Kamar <kamarster@gmail.com>
2018-11-01 17:21:56 +0300

    [Feature #161585219] Add modal functionality

---
+++ UI/static/js/main.js
@@ -0,0 +1,29 @@
+// Get the login modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Get the signup modal
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Get the login modal
+var modal = document.getElementById('id03');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
