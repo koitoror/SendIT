6a4f088c6fe3ca604f53ab30b421f7782d05ca3c
Daniel Kamar <kamarster@gmail.com>
2018-11-08 11:28:03 +0300

    [Chore #161738027] Initialize config package

---
+++ instance/config.py
@@ -0,0 +1,35 @@
+# config.py
+import os
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.getenv('SECRET_KEY' or 'some-very-long-string-of-random-characters-CHANGE-TO-YOUR-LIKING')
+    # SECRET_KEY="some-very-long-string-of-random-characters-CHANGE-TO-YOUR-LIKING"
+    # FLASK_CONFIG= 'development'
+    # RESTPLUS_VALIDATE = True
+    # ERROR_404_HELP = False
+
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+    MODE="development"
+
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    DEBUG = True
+    # db_name = os.getenv("TEST_DB")
+    MODE="testing"
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+}
