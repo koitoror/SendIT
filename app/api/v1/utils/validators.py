37d9e9a761e9b433dcb1eae9feb48766e7e9f6d9
Daniel Kamar <kamarster@gmail.com>
2018-11-08 12:21:46 +0300

    [Feature #161739482] Add validators

---
+++ app/api/v1/utils/validators.py
@@ -0,0 +1,111 @@
+import re
+
+def validate_user_data(user):
+    """ this funtion validates the user data """
+
+    user['username'] = user['username'].lower()
+
+    if user['username'] == '':
+        return {'warning': 'username is a required field'}, 400
+
+    # Check for empty email
+    elif user['email'] == '':
+        return {'warning': 'email is a required field'}, 400
+
+    # Check for empty password
+    elif user['password'] == '':
+        return {'warning': 'password is a required field'}, 400
+
+    elif user['password'] == 'password':
+        return {'warning': "password cannot be 'password'"}, 400
+
+    elif user['password'].strip(' ').isdigit():
+        return {'warning': 'password should be alphanumeric'}, 400
+
+    elif user['password'] != user['confirm']:
+        return {'warning': 'password mismatch!'}, 400
+
+    # Check for a valid user name
+    if not re.match(r'^[a-zA-Z0-9_.+-]+$', user['username'].strip(' ')):
+        return {'warning': 'Enter a valid username'}, 400
+
+    if user['username'].strip(' ').isdigit():
+        return {'warning': 'Enter a non digit username'}, 400
+
+    # Check for a valid email
+    if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
+            user['email'].strip(' ')):
+        return {'warning': 'Enter a valid email address'}, 400
+
+    # check for a valid password
+    if not user["password"].strip():
+        return {"warning": "Enter a valid password"}, 400
+
+    # Check for large/long inputs
+    if len(user['username']) > 15:
+        return {'warning': 'username is too long'}, 400
+
+    elif len(user['email']) > 50:
+        return {'warning': 'email is too long'}, 400
+
+    elif len(user['password']) < 6:
+        return {'warning': 'password requires atlest 6 characters'}, 400
+
+def validate_parcel_data(parcel):
+    """ this funtion validates the parcel data """
+
+    # Check for empty parcel_name
+    if parcel['parcel_name'] == ' ':
+        return {'warning': 'parcel_name is a required field'}, 400
+
+    # Check for empty destination_location
+    elif parcel['destination_location'] == ' ':
+        return {'warning': 'destination_location is a required field'}, 400
+    
+    # check for a valid parcel_name
+    if parcel['parcel_name'].strip(' ').isdigit():
+        return {'warning': 'Enter a non digit parcel_name'}, 400
+
+    if not parcel["parcel_name"].strip():
+        return {"warning": "Enter a valid parcel_name"}, 400
+    
+    # check for valid destination_location
+    if parcel['destination_location'].strip(' ').isdigit():
+        return {'warning': 'Enter non digit destination_location'}, 400
+
+    if not parcel["destination_location"].strip():
+        return {"warning": "Enter valid destination_location"}, 400
+
+    # Check for large/long inputs
+    if len(parcel['parcel_name']) > 50:
+        return {'warning': 'parcel_name is too long'}, 400
+
+
+def validate_update_parcel(parcel, data):
+    """ this funtion validates the updated parcel data """
+
+    # Check for empty parcel_name
+    if data['parcel_name'] == '':
+        data['parcel_name'] = parcel['parcel_name']
+
+    # Check for empty destination_location
+    if data['destination_location'] == '':
+        data['destination_location'] = parcel['destination_location']
+    
+    # check for a valid parcel_name
+    if data['parcel_name'].strip(' ').isdigit():
+        return {'warning': 'Enter a non digit parcel_name'}, 400
+
+    if not data["parcel_name"].strip():
+        return {"warning": "Enter a valid parcel_name"}, 400
+    
+    # check for valid destination_location
+    if data['destination_location'].strip(' ').isdigit():
+        return {'warning': 'Enter non digit destination_location'}, 400
+
+    if not data["destination_location"].strip():
+        return {"warning": "Enter valid destination_location"}, 400
+
+    # Check for large/long inputs
+    if len(data['parcel_name']) > 50:
+        return {'warning': 'parcel_name is too long'}, 400
