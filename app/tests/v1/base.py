c425ffe5c2312aa9d5722050bdd677771978a55c
Daniel Kamar <kamarster@gmail.com>
2018-11-09 17:34:37 +0300

    [Chore #161738027] Restructure directory

---
+++ app/tests/v1/base.py
@@ -0,0 +1,60 @@
+from flask_testing import TestCase
+from app.api.v1.models.parcel import parcel_class
+from app.api.v1.models.user import user_class
+
+from run import app
+
+# user_class = user_class
+# parcel_class = parcel_class
+
+class BaseTestCase(TestCase):
+    """ Base Tests """
+
+    @classmethod
+    def create_app(cls):
+        app.config.from_object('instance.config.TestingConfig')
+        return app
+
+    def setUp(self):
+        self.user_class = user_class
+        self.data = self.user_class.create_user({
+            "confirm": "johndoe123",
+            "password": "johndoe123",
+            "email": "johndoe@gmail.com",
+            "username": "johndoe"
+            })
+
+        self.user_class.user_id = ''
+
+        self.parcel_class = parcel_class
+        self.data = self.parcel_class.create_parcel_test({
+            "parcel_name": "TRAVEL BAG", 
+            "pickup_location": "DROP VAN",
+            "destination_location": "MOMBASA",
+            "price": 1000
+            })
+        
+        self.no_parcel_name = self.parcel_class.create_parcel_test({
+            "parcel_name": " ", 
+            "pickup_location": "DROP VAN",
+            "destination_location": "MOMBASA",
+            "price": 1000
+            })
+
+        self.no_price = self.parcel_class.create_parcel_test({
+            "parcel_name": "TRAVEL BAG", 
+            "pickup_location": "DROP VAN",
+            "destination_location": "MOMBASA"
+
+            })
+
+        self.no_destination_location = self.parcel_class.create_parcel_test({
+            "parcel_name": "TRAVEL BAG", 
+            "pickup_location": "DROP VAN",
+            "destination_location": " ",
+            "price": 1000
+            })
+
+    def tearDown(self):
+        self.user_class.no_of_users.clear()
+        self.parcel_class.no_of_parcels.clear()
