25f72f36ffc295c806e1b30bb7ce8feee9889d9a
Daniel Kamar <kamarster@gmail.com>
2018-11-09 17:12:49 +0300

    [Feature #161738123] Add admin token auth

--- app/api/v1/utils/decorators.py
+++ app/api/v1/utils/decorators.py
@@ -40,6 +40,34 @@ def token_required(f):
     return wrap
 
 
+def token_required1(f):
+    """Ensures user is logged in before action
+    """
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        token = None
+        # user_id = ""
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            api.abort(400, "Token Missing")
+        try:
+            payload = jwt.decode(token, current_app.config.get("SECRET_KEY"))
+            user_id = payload['sub']
+
+        except jwt.ExpiredSignatureError:
+            api.abort(400, "Token has expired. Please login again")
+
+        except jwt.InvalidTokenError:
+            api.abort(400, "Invalid token")
+
+        # return f(*args, **kwargs)
+        return f(user_id, *args, **kwargs)
+
+    return wrap
+
+
 def user_required(f):
     """Checks for authenticated users with valid token in the header"""
 
