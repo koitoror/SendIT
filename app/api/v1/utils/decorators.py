bc1d1870094e0bae06f1adad9cab5db50fff88dc
Daniel Kamar <kamarster@gmail.com>
2018-11-15 18:27:58 +0300

    [Bug 161977372] Simplifying decorators

--- app/api/v1/utils/decorators.py
+++ app/api/v1/utils/decorators.py
@@ -1,5 +1,6 @@
-"""Contains the token_required decorator to restrict access to authenticated users only and
-the admin_required decorator to restrict access to administrators only.
+"""Contains the token_required decorator to restrict access
+to authenticated users only and the admin_required decorator
+to restrict access to administrators only.
 """
 
 from functools import wraps
@@ -7,9 +8,8 @@ from functools import wraps
 from flask import request, current_app, jsonify, make_response
 import jwt
 
-#local imports
+# local imports
 from ..utils.udto import api
-from instance import config
 
 
 def user_required(f):
@@ -34,35 +34,7 @@ def user_required(f):
         except jwt.InvalidTokenError:
             api.abort(400, "Invalid token")
 
-        # return f(*args, **kwargs)
         return f(user_id, *args, **kwargs)
-
-    return wrap
-
-def token_required(f):
-    """Ensures user is logged in before action
-    """
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        token = None
-        user_id = ""
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-
-        if not token:
-            api.abort(400, "Token Missing")
-        try:
-            payload = jwt.decode(token, current_app.config.get("SECRET_KEY"))
-            user_id = payload['sub']
-
-        except jwt.ExpiredSignatureError:
-            api.abort(400, "Token has expired. Please login again")
-
-        except jwt.InvalidTokenError:
-            api.abort(400, "Invalid token")
-
-        return f(*args, **kwargs)
-        # return f(user_id, *args, **kwargs)
 
     return wrap
 
@@ -73,7 +45,6 @@ def admin_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         token = None
-        # user_id = ""
         admin = ""
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token']
@@ -82,10 +53,11 @@ def admin_required(f):
             api.abort(400, "Token Missing")
         try:
             payload = jwt.decode(token, current_app.config.get("SECRET_KEY"))
-            if payload['admin'] != True:
+            # user_id = payload['sub']
+            if payload['admin'] is not True:
                 return make_response(jsonify({
-                    "message" : "Not authorized to perform this function as a non-admin"}), 401)
-
+                    "message": "Not authorized to perform this function"}),
+                    401)
 
         except jwt.ExpiredSignatureError:
             api.abort(400, "Token has expired. Please login again")
@@ -93,56 +65,6 @@ def admin_required(f):
         except jwt.InvalidTokenError:
             api.abort(400, "Invalid token")
 
-        # return f(*args, **kwargs)
         return f(admin, *args, **kwargs)
 
     return wrap
-
-
-# def user_required(f):
-#     """Checks for authenticated users with valid token in the header"""
-
-#     @wraps(f)
-#     def decorated(*args, **kwargs):
-#         """validate token provided"""
-#         token = None
-
-#         if 'x-access-token' in request.headers:
-#             token = request.headers['x-access-token']
-
-#         try:
-#             data = jwt.decode(token, config.Config.SECRET_KEY) # pylint: disable=W0612
-#         except:
-#             return make_response(jsonify({
-#                 "message" : "kindly provide a valid token in the header"}), 401)
-
-#         return f(*args, **kwargs)
-
-#     return decorated
-
-# def admin_required(f):
-#     """Checks for authenticated admins with valid token in the header"""
-
-#     @wraps(f)
-#     def decorated(*args, **kwargs):
-#         """validate token provided and ensures the user is an admin"""
-#         token = None
-
-#         if 'x-access-token' in request.headers:
-#             token = request.headers['x-access-token']
-
-#         try:
-#             data = jwt.decode(token, config.Config.SECRET_KEY)
-#             if data['usertype'] != "admin":
-#                 return make_response(jsonify({
-#                     "message" : "Not authorized to perform this function as a non-admin"}), 401)
-
-#         except:
-#             return make_response(jsonify({
-#                 "message" : "kindly provide a valid token in the header"}), 401)
-
-#         return f(*args, **kwargs)
-
-#     return decorated
-
-
