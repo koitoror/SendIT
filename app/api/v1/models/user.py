79275a5d7d692f8c2350e91245097c7d393a11c2
Daniel Kamar <kamarster@gmail.com>
2018-11-15 18:26:19 +0300

    [Chore 161977372] Refactor to remove commented code

--- app/api/v1/models/user.py
+++ app/api/v1/models/user.py
@@ -3,8 +3,6 @@ from datetime import datetime, timedelta
 # local imports
 import jwt
 from flask import current_app
-from flask_bcrypt import Bcrypt
-from ..utils.udto import api
 
 
 class User(object):
@@ -12,7 +10,6 @@ class User(object):
 
     def __init__(self):
         """constructor method"""
-        # import pdb;pdb.set_trace()
         self.no_of_users = [{
             "user_id": 1,
             "confirm": "johndoe123",
@@ -22,64 +19,38 @@ class User(object):
             "admin": True
         }]
 
-    # @staticmethod
-    # def generate_token1(self, user_id):
-    #     """token generation for authentication"""
-    #     try:
-    #         payload = {
-    #             'id': user_id,
-    #             # 'usertype': User.get_one(self, user_id)['usertype'],
-    #             "exp":datetime.utcnow() + timedelta(days=1),
-    #             "iat":datetime.utcnow()
-    #         }
-    #         return jwt.encode(payload, current_app.config.get("SECRET_KEY"))
-    #     except Exception as e:
-    #         return {"message": str(e)}
-
     @staticmethod
     def generate_token(user_id):
         """token generation for authentication"""
         try:
-            payload = {"exp":datetime.utcnow() + timedelta(days=1),
-                       "iat":datetime.utcnow(),
-                       "sub":user_id}
+            payload = {"exp": datetime.utcnow() + timedelta(days=1),
+                       "iat": datetime.utcnow(),
+                       "sub": user_id}
             return jwt.encode(payload, current_app.config.get("SECRET_KEY"))
         except Exception as e:
             return {"message": str(e)}
-
 
     @staticmethod
     def generate_token1(user_id, admin):
         """token generation for authentication"""
         try:
-            payload = {"exp":datetime.utcnow() + timedelta(days=1),
-                       "iat":datetime.utcnow(),
+            payload = {"exp": datetime.utcnow() + timedelta(days=1),
+                       "iat": datetime.utcnow(),
                        "admin": admin,
-                       "sub":user_id}
+                       "sub": user_id}
             return jwt.encode(payload, current_app.config.get("SECRET_KEY"))
         except Exception as e:
             return {"message": str(e)}
 
-
     def create_user(self, data):
         """Method for creating an user"""
-
         data["user_id"] = int(len(self.no_of_users) + 1)
-        data["date_registered"] = str(datetime.now().strftime('%b-%d-%Y : %H:%M:%S'))
-        # data["usertype"] = "user"
+        data["date_registered"] = str(
+            datetime.now().strftime('%b-%d-%Y : %H:%M:%S'))
         data['admin'] = False
         self.no_of_users.append(data)
         return data
-        # return self.no_of_users
 
-    # def get_one(self, user_id):
-    #     """Method for fetching one user by its id"""
-    #     user = [user for user in self.no_of_users if user["user_id"] == user_id]
-
-    #     if not user:
-    #         api.abort(404, "User {} does not exist".format(user_id))
-    #     return user
-    
     def get_user_by_email(self, email):
         """Method for fetching one user by its email"""
         user = [user for user in self.no_of_users if user["email"] == email]
@@ -87,55 +58,9 @@ class User(object):
 
     def get_user_by_username(self, username):
         """Method for fetching one user by its username"""
-        user = [user for user in self.no_of_users if user["username"] == username]
+        user = [user for user in self.no_of_users
+                if user["username"] == username]
         return user
 
-    # def get_user_by_id(self, user_id):
-    #     """Method for fetching one user by its username"""
-    #     user = [user for user in self.no_of_users if user["user_id"] == user_id]
-    #     return user
-
-    # # def is_admin(self, user_id):
-    # #     """Method for fetching one user by its username"""
-    # #     user = [user for user in self.no_of_users if user["admin"] = True and ]
-    # #     if not user:
-    # #         api.abort(401, 'you cannot perform the opertion')
-    # #     return user
-
-    # def get_user_by_password(self, password):
-    #     """Method for fetching one user by its password"""
-    #     user = [user for user in self.no_of_users if user["password"] == password]
-    #     return user
-
-    # def delete_user(self, user_id):
-    #     "Method for deleting an user"
-
-    #     user = self.get_one(user_id)
-    #     self.no_of_users.remove(user[0])
-
-    # def update_user(self, user_id, data):
-    #     """Method for updating an user"""
-
-    #     user = self.get_one(user_id)
-    #     data['date_promoted'] = str(datetime.now().strftime('%b-%d-%Y : %H:%M:%S'))
-    #     # data["usertype"] = "admin"
-    #     user[0].update(data)
-    #     return user
-
-    # def promote_user(self, user_id, data):
-    #     """Method for promoting a user to admin"""
-
-    #     user = self.get_one(user_id)
-    #     data['date_promoted'] = str(datetime.now().strftime('%b-%d-%Y : %H:%M:%S'))
-    #     data["admin"] = True
-    #     user[0].update(data)
-    #     return user
-
-    # def get_all(self):
-    #     """Method for returning all users."""
-    #     users = [users for users in self.no_of_users]
-    #     if not users:
-    #         api.abort(404, "No Users Found.")
-    #     return users
 
 user_class = User()
