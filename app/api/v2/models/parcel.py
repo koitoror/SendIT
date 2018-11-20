2c9f19238dc48f45fc1f8bfb44288eff7dced2b2
Daniel Kamar <kamarster@gmail.com>
2018-11-20 22:27:26 +0300

    [Feature #162030525] Add get parcel by id method

--- app/api/v2/models/parcel.py
+++ app/api/v2/models/parcel.py
@@ -15,3 +15,14 @@ class Parcel(object):
     def create_parcel(cursor, parcel_name, price, pickup_location, destination_location, status, user_id):
         query = "INSERT INTO parcels (parcel_name, price, pickup_location, destination_location, status, user_id) VALUES (%s, %s, %s, %s, %s, %s)"
         cursor.execute(query, (parcel_name, price, pickup_location, destination_location, status, user_id))
+
+    @staticmethod
+    def get_parcel_by_id(dict_cursor, parcel_id):
+        query_string="SELECT * FROM parcels WHERE parcel_id=%s"
+        dict_cursor.execute(query_string, [parcel_id])
+        data = dict_cursor.fetchone()
+        if not data:
+            api.abort(404, "Parcel {} not found".format(parcel_id))
+        parcel = {key:str(value) for key, value in data.items() if value is not str}
+        return parcel
+     
