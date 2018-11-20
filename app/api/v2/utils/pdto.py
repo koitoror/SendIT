96f8593964a28e74cfcea7d02e538fbd10e26cec
Daniel Kamar <kamarster@gmail.com>
2018-11-20 22:17:52 +0300

    [Feature #162030527] Add the parcel model dto

---
+++ app/api/v2/utils/pdto.py
@@ -0,0 +1,96 @@
+from flask_restplus import Namespace, fields, reqparse
+
+
+api = Namespace("parcels", description="parcels related operations")
+
+parcel = api.model(
+    "parcels", {
+        "parcel_id": fields.Integer(),
+        "user_id": fields.Integer(),
+        "date_ordered": fields.String(),
+        "date_modified": fields.String(),
+        "parcel_name": fields.String(
+            required=True, description="The parcel name",
+            example="TRAVEL BAG"),
+        "price": fields.Integer(
+            required=True, description="The parcel price",
+            example=100),
+        "present_location": fields.String(
+            required=True, description="The parcel present_location",
+            example="CITY OFFICE"),
+        "pickup_location": fields.String(
+            required=True, description="The parcel pickup_location",
+            example="DROP VAN"),
+        "destination_location": fields.String(
+            required=True, description="The parcel destination_location",
+            example="MOMBASA"),
+        "status": fields.String(
+            required=True, description="The parcel status",
+            example="UNDELIVERED"),
+        "cancel_order": fields.Boolean(
+            required=True, description="The parcel cancel delivery order",
+            example=False)
+    
+    })
+
+post_parcel = api.model(
+    "post_parcel", {
+        "parcel_name": fields.String(
+            "parcels name", example="TRAVEL BAG"),
+        "price": fields.Integer(
+            "parcels price", example=1000),
+        "pickup_location": fields.String(
+            "parcels pickup_location", example="DROP VAN"),
+        "destination_location": fields.String(
+            "parcels destination_location", example="MOMBASA"),
+        "status": fields.String(
+            "parcels status", example="IN TRANSIT"),
+
+    })
+
+update_parcels_admin = api.model(
+    "update_parcels_admin", {
+        "present_location": fields.String(
+            "parcels present_location", example="DISPATCH CENTRE"),
+        "status": fields.String(
+            "parcels status", example="IN TRANSIT")
+
+    })
+
+update_parcels_admin_pl = api.model(
+    "update_parcels_admin", {
+        "present_location": fields.String(
+            "parcels present_location", example="DISPATCH CENTRE")
+
+    })
+
+update_parcels_admin_status = api.model(
+    "update_parcels_admin", {
+        "status": fields.String(
+            "parcels status", example="IN TRANSIT")
+
+    })
+
+update_parcels_user = api.model(
+    "update_parcels_user", {
+        "cancel_order": fields.Boolean(
+            "parcels cancel_order", example=True),
+        "destination_location": fields.String(
+            "parcels destination_location", example="MOMBASA")
+
+    })
+
+update_parcels_user_cancel = api.model(
+    "update_parcels_user_cancel", {
+        "cancel_order": fields.Boolean(
+            "parcels cancel_order", example=True)
+
+    })
+
+update_parcels_user_destination = api.model(
+    "update_parcels_user_destination", {
+        "destination_location": fields.String(
+            "parcels destination_location", example="MOMBASA")
+
+    })
+
