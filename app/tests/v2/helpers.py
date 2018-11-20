9b3bcc8ee1679d22c1e5718cdc542d3efa6a6bbe
Daniel Kamar <kamarster@gmail.com>
2018-11-20 12:35:12 +0300

    [Chore #162076312] Remove commented code

--- app/tests/v2/helpers.py
+++ app/tests/v2/helpers.py
@@ -23,24 +23,3 @@ def login_user(self):
         )),
         content_type='application/json'
     )
-
-# def create_parcel(self, access_token):
-#     "helper function for creating an parcel"
-#     rv = self.client.post(
-#     'api/v2/parcels',
-#     headers={
-#         "x-access-token": access_token,
-#         "content-type": "application/json"
-#     },
-#     data=self.parcel
-#     )
-
-# def get_all(self, access_token):
-#     "helper function for creating an parcel"
-#     rv = self.client.get(
-#     'api/v2/parcels',
-#     headers={
-#         "x-access-token": access_token,
-#         "content-type": "application/json"
-#     }
-#     )
