b0db5e92e019bbd7e8dbceda03e50a83e500a969
Daniel Kamar <kamarster@gmail.com>
2018-11-23 09:06:56 +0300

    [Feature #162076312] Add register and login of admin inside tests

--- app/tests/v2/helpers.py
+++ app/tests/v2/helpers.py
@@ -1,4 +1,11 @@
 import json
+from flask_bcrypt import Bcrypt
+
+
+from app.api.v2.models.user import User
+from app.database import Database
+db = Database()
+cursor = db.cursor
 
 def register_user(self):
     """helper function for registering a user."""
@@ -12,6 +19,15 @@ def register_user(self):
         )),
         content_type='application/json'
         )
+def register_admin(self):
+    """helper function for registering a user."""
+    username='kamar',
+    email='kamardaniel@gmail.com',
+    password='password123',
+    hash_password = Bcrypt().generate_password_hash(password).decode()
+    admin = True
+    User.create_admin(cursor, username=username, email=email, password=hash_password, admin=admin)
+    pass
 
 def login_user(self):
     """helper function for login a user."""
@@ -19,6 +35,17 @@ def login_user(self):
         'api/v2/auth/login',
         data=json.dumps(dict(
             username='kamar',
+            password='password123'
+        )),
+        content_type='application/json'
+    )
+
+def login_admin(self):
+    """helper function for login an admin."""
+    return self.client.post(
+        'api/v2/auth/login',
+        data=json.dumps(dict(
+            username='admin',
             password='password123'
         )),
         content_type='application/json'
