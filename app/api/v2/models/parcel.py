e8021f8408302591f10c3357537d65c80289c724
Daniel Kamar <kamarster@gmail.com>
2019-01-26 02:37:37 +0300

    [Chore #162042793] Refactor for JS Frontend

--- app/api/v2/models/parcel.py
+++ app/api/v2/models/parcel.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from ..utils.pdto import api
 
 
@@ -28,13 +30,29 @@ class Parcel(object):
             api.abort(404, "Parcel {} not found".format(parcel_id))
         parcel = {key: str(value)
                   for key, value in data.items() if value is not str}
-        return parcel
+
+
+        obj = {                                         
+            "parcel_id": parcel["parcel_id"],
+            "parcel_name": parcel["parcel_name"],
+            "pickup_location": parcel["pickup_location"],
+            "destination_location": parcel["destination_location"],
+            "price": parcel["price"],
+            "status": parcel["status"],
+            "user_id": parcel["user_id"],
+            "date_ordered": datetime.strptime(parcel["date_ordered"], '%Y-%m-%d %H:%M:%S.%f+03:00').strftime('%d-%b-%Y : %H:%M:%S')
+        }
+
+        return obj
 
     @staticmethod
     def modify_parcel_user_cancel(dict_cursor, cursor, status, date_modified, parcel_id, user_id):
         data = Parcel.get_parcel_by_id(dict_cursor, parcel_id)
         if data["user_id"] != str(user_id):
             api.abort(401, "Unauthorized to edit this parcel order")
+        if data["status"] == "DELIVERED":
+            api.abort(401, "Parcel order cannot be canceled since it has been delivered to the destination!")
+
         query = "UPDATE parcels SET status=%s, date_modified=%s WHERE (parcel_id=%s)"
         cursor.execute(query, (status, date_modified, parcel_id))
 
@@ -48,17 +66,14 @@ class Parcel(object):
 
     @staticmethod
     def modify_parcel_admin_pl(dict_cursor, cursor, present_location, parcel_id):
-        # data = Parcel.get_parcel_by_id(dict_cursor, parcel_id)
-        # if data["user_id"] != str(user_id):
-        #     api.abort(401, "Unauthorized to edit this parcel order")
         query = "UPDATE parcels SET present_location=%s WHERE (parcel_id=%s)"
         cursor.execute(query, (present_location, parcel_id))
 
     @staticmethod
     def modify_parcel_admin_status(dict_cursor, cursor, status, parcel_id):
-        # data = Parcel.get_parcel_by_id(dict_cursor, parcel_id)
-        # if data["user_id"] != str(user_id):
-        #     api.abort(401, "Unauthorized to edit this parcel order")
+        data = Parcel.get_parcel_by_id(dict_cursor, parcel_id)
+        if data["status"] == "DELIVERED":
+            api.abort(401, "Parcel order cannot be canceled since it has been delivered to the destination!")
         query = "UPDATE parcels SET status=%s WHERE (parcel_id=%s)"
         cursor.execute(query, (status, parcel_id))
 
@@ -84,7 +99,9 @@ class Parcel(object):
                 "parcel_id": parcel["parcel_id"],
                 "parcel_name": parcel["parcel_name"],
                 "pickup_location": parcel["pickup_location"],
+                "present_location": parcel["present_location"],
                 "destination_location": parcel["destination_location"],
+                "cancel_order": parcel["cancel_order"],                
                 "price": parcel["price"],
                 "status": parcel["status"],
                 "user_id": parcel["user_id"],
@@ -104,7 +121,9 @@ class Parcel(object):
                 "parcel_id": parcel["parcel_id"],
                 "parcel_name": parcel["parcel_name"],
                 "pickup_location": parcel["pickup_location"],
+                "present_location": parcel["present_location"],
                 "destination_location": parcel["destination_location"],
+                "cancel_order": parcel["cancel_order"],                
                 "price": parcel["price"],
                 "status": parcel["status"],
                 "user_id": parcel["user_id"],
