41bedab2adc36d9a531e0408a5aefac8451bce0b
Daniel Kamar <kamarster@gmail.com>
2019-01-26 02:25:05 +0300

    [Feature #162042864] Enable the flask mail feature

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
 
-    # mail = Mail(app)
+    mail = Mail(app)
 
     # registering the blueprint
     app.register_blueprint(v2)
