a1e0ce71442fb2b56887ff63b7a4c28724d30c6e
Daniel Kamar <kamarster@gmail.com>
2019-01-26 02:50:53 +0300

    [Chore #162042746] Update travis imports

---
+++ travis.sh
@@ -0,0 +1,9 @@
+export SECRET_KEY="some-very-long-string-of-random-characters-CHANGE-TO-LIKING"
+export FLASK_CONFIG="development"
+export FLASK_APP="run.py run"
+export GMAIL_MAIL="atsilikam@gmail.com"
+export GMAIL_USERNAME="atsilikam"
+export GMAIL_PASSWORD="eldoret17"
+export TEST_DB_URL='dbname=send_it_test password=123456 host=localhost port=5432 user=postgres'
+export DATABASE_URL='dbname=send_it password=123456 host=localhost port=5432 user=postgres'
+export PROD_DB_URL='database=dfdao6n5c3ldv0 password=10030c356eb6d7f42f498a9b83cc66e050056e840e8164a0ff3ba99376612ae7 host=ec2-54-197-249-140.compute-1.amazonaws.com port=5432 user=lvyhayqrcwomlj'
