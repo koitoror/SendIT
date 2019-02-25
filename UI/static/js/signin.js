c93a38a0ef49e83567dd378e9d0050dc3c766aff
Daniel Kamar <kamarster@gmail.com>
2019-02-25 17:39:41 +0300

    [Feature #163167251] Code refactor for adding login with assign timeout

--- UI/static/js/signin.js
+++ UI/static/js/signin.js
@@ -1,10 +1,11 @@
 /*jshint esversion: 6 */
 
 import { api } from './api'
+
+// export const signI = () => {
 const signin = document.querySelector("#signIn")
 const success = document.querySelector("#success")
 const warning = document.querySelector("#warning")
-
 signin.addEventListener('submit', e =>  {
     e.preventDefault()
     signin.classList.add("loading");
@@ -23,22 +24,26 @@ signin.addEventListener('submit', e =>  {
         if (data.admin == true){
             localStorage.setItem("token", data.token)
             localStorage.setItem("username", username)
-            localStorage.setItem("user_id", data.user_id)            
+            localStorage.setItem("user_id", data.user_id)
             warning.classList.add('hide')
             success.classList.remove('hide')
             success.classList.add('show')
             success.innerHTML = data.message
-            setTimeout(() =>{window.location.href = './dashboard.html'},2000)
+            // setTimeout(() =>{window.location.href = './dashboard.html'}, 2000)
+            setTimeout(() => window.location.assign('./dashboard.html'), 2000)
+
         }
-        if (data.admin == false){
+        else if (data.admin == false){
             localStorage.setItem("token", data.token)
             localStorage.setItem("username", username)
-            localStorage.setItem("user_id", data.user_id)            
+            localStorage.setItem("user_id", data.user_id)
             warning.classList.add('hide')
             success.classList.remove('hide')
             success.classList.add('show')
             success.innerHTML = data.message
-            setTimeout(() =>{window.location.href = './profile.html'},2000)
+            // setTimeout(() =>{window.location.href = './profile.html'}, 2000)
+            setTimeout(() => window.location.assign('./profile.html'), 2000)
+
         }
         else{
             success.classList.add('hide')
@@ -48,3 +53,6 @@ signin.addEventListener('submit', e =>  {
         }
     } )
 })
+// }
+
+// module.exports = signIn
