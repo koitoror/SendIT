40046f4c8d19f4cea4630b0d0c7cebc676d515d2
Daniel Kamar <kamarster@gmail.com>
2019-02-25 17:40:40 +0300

    [Feature #163167251] Code refactor for additional modal for redirect js

--- UI/static/js/modal.js
+++ UI/static/js/modal.js
@@ -25,3 +25,28 @@ document.getElementById("#login_instead").click(function() {
     document.getElementById("#id02").modal('hide');
     document.getElementById("#id01").modal('show');
 });
+
+// Get the change modal
+var modal = document.getElementById('id03');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+// Get the change2 modal
+var modal = document.getElementById('id04');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+document.getElementById("#_instead").click(function() {
+    document.getElementById("#id04").modal('hide');
+    document.getElementById("#id03").modal('show');
+});
