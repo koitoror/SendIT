692631b2098671da586f86c9a7a10f32a0202794
Daniel Kamar <kamarster@gmail.com>
2019-02-25 17:41:51 +0300

    [Feature #163167251] Add JS for modifying parcel order status

---
+++ UI/static/js/editOrderStatus.js
@@ -0,0 +1,55 @@
+/*jshint esversion: 6 */
+
+import { api } from "./api";
+
+function getQueryParameter(key) {
+    let url = new URL(window.location.href);
+    return url.searchParams.get(key);
+}
+let parcel_id = getQueryParameter('id');
+
+// api.get(`/parcels/${parcel_id}`)
+// .then(res => res.json())
+// .then(data => {
+//     document.getElementById("parcel-name").value = data['parcel_name'];
+//     document.getElementById("status").value = data['status'];
+
+// });
+
+const success = document.querySelector("#success");
+const warning = document.querySelector("#warning");
+
+const form = document.querySelector("#editForm");
+
+let user = document.querySelector("#username")
+user.innerHTML = localStorage.getItem("username");
+
+
+form.addEventListener("submit", e =>{
+    e.preventDefault();
+    let parcel_name = document.getElementById("parcel-name").value;
+    let status = document.getElementById("status").value;
+
+    const data = {
+        parcel_name,
+        status
+    };
+    
+    api.update(`/parcels/${parcel_id}/status`, data)
+    .then(res => res.json())
+    .then(data => {
+        if(data.message == "Status updated successfully"){
+            warning.classList.add('hide');
+            success.classList.remove('hide');
+            success.classList.add('show');
+            success.innerHTML = data.message;
+            setTimeout(() =>{window.location.href = './dashboard.html';},2000);
+        }else{
+            success.classList.add('hide');
+            warning.classList.remove('hide');
+            warning.classList.add('show');
+            warning.innerHTML = data.message;
+        }
+        
+    });
+});
